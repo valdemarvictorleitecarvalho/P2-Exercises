215191efbc29b5b786f19bafc4d415c5e33b8c0a
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2024-02-01 11:34:07 -0300

    O primeiro codigo Java

---
+++ exercicio/IdadePreferencial.java
@@ -0,0 +1,11 @@
+public class IdadePreferencial {
+    public static void main(String[] args) {  // Definindo uma função
+        int idade = 20;
+        if (idade >= 60) {
+            System.out.println("Voce tem " + idade
+                   + " anos. Voce pode usar o atendimento especial.");
+        } else {
+        System.out.println("Voce tem " + idade + " anos. Voce ainda nao pode usar o atendimento especial.");
+        }
+    }
+}
