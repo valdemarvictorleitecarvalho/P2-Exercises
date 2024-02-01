4e3c7bd20de5fa4c321db8c66141528e5829df09
valdemarvictorleitecarvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2024-02-01 14:33:35 -0300

    Comando de Desvio

--- exercicio/lp2/lab01/IdadePreferencialInteligente.java
+++ exercicio/lp2/lab01/IdadePreferencialInteligente.java
@@ -2,12 +2,33 @@ package lp2.lab01;
 
 public class IdadePreferencialInteligente {
     public static void main(String[] args) {  // Definindo uma função
-        int idade = 20;
-        if (idade >= 60) {
+        
+	    int anoAtual = 2024, anoNascimento = 2005, qtCriancasDeColo = 3;
+
+    	    int idade = anoAtual - anoNascimento;
+
+	    boolean gravida = false;	    
+            boolean criancaDeColo = true;
+
+	    if (idade >= 60) {
             System.out.println("Voce tem " + idade
-                   + " anos. Voce pode usar o atendimento especial.");
-        } else {
-        System.out.println("Voce tem " + idade + " anos. Voce ainda nao pode usar o atendimento especial.");
-        }
+                   + " anos. Preferencial Idoso.");
+        
+	    } 
+	    else {
+		    if (gravida) {
+			    System.out.println("Voce tem " + idade 
+					    + " anos e esta gravida. Preferencial Gestante.");
+       		    }
+		    else if (criancaDeColo) {
+			    System.out.println("Voce tem " + idade + 
+					    " anos. Preferencial pois esta com " + qtCriancasDeColo 
+					    + " criancas de colo.");
+	            }
+		    else {
+		    System.out.println("Voce tem " + idade 
+				    + " anos. Voce ainda nao pode usar o atendimento especial.");
+         	    }
+	}
     }
 }
